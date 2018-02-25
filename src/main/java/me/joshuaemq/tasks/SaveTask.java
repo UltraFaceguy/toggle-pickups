db3a01fbbce904020c59f2962457350b7626efc8
UltraFaceguy <kattreinkyle@gmail.com>
2018-02-24 23:15:09 -0500

    Leaving SECRET INFO HINTS

--- src/main/java/me/joshuaemq/tasks/SaveTask.java
+++ src/main/java/me/joshuaemq/tasks/SaveTask.java
@@ -3,7 +3,9 @@ package me.joshuaemq.tasks;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 import me.joshuaemq.TogglePickupsPlugin;
+import me.joshuaemq.data.PlayerFilterData;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -17,6 +19,29 @@ public class SaveTask extends BukkitRunnable {
   }
 
   public void run() {
+
+    // This right here pulls from the global list of saved playerdata;
+    plugin.getPlayerFilterManager().getPlayerFilterMap();
+
+    // You can do things like this
+    PlayerFilterData data = plugin.getPlayerFilterManager().getPlayerFilterMap().get(UUID.fromString("PLAYERS-UUID-HERE"));
+    System.out.println("Is player's filter on?: " + data.isFilterEnabled());
+    System.out.println("All of the player's added filters: " + data.getLootFilterEntries());
+
+    // To edit those values, we'll need to update them in the manager...
+    PlayerFilterData data2 = plugin.getPlayerFilterManager().getPlayerFilterMap().get(UUID.fromString("PLAYERS-UUID-HERE"));
+    List<String> filterEntries = new ArrayList<>();
+    filterEntries.add("dank");
+    filterEntries.add("memes");
+    data2.setFilterEnabled(false);
+    data2.setLootFilterEntries(filterEntries);
+    plugin.getPlayerFilterManager().getPlayerFilterMap().put(UUID.fromString("PLAYERS-UUID-HERE"), data2);
+
+    // Ideally, you'd use this save task to loop through all the things in that manager's filter map
+    // and save them all to config, then remove all non-online players from the filter map.
+    // Also it should be noted that there was a bug before, that made the plugin only work for
+    // one player! Use the maps to prevent this :O
+
     if (plugin.get != null) {
 
       Collection<? extends Player> getOnlinePlayers = Bukkit.getOnlinePlayers();
